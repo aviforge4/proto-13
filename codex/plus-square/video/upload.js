feb932c79bd4c6c6f8ef921baaf7a258188f93b2
aviforge4 <mydevicehp1@gmail.com>
2024-07-13 14:59:29 +0530

    Add files via upload

---
+++ codex/plus-square/video/upload.js
@@ -0,0 +1,7 @@
+document.getElementById('addVideoButton').addEventListener('click', function () {
+    document.getElementById('videoUploadForm').style.display = 'block';
+});
+
+document.getElementById('closeVideoUploadForm').addEventListener('click', function () {
+    document.getElementById('videoUploadForm').style.display = 'none';
+});
